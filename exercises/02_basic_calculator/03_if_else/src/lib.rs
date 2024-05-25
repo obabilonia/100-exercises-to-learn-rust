0fadf8765c32f57b5b1e8242a51bca37917d2aa6
Oscar Andres Babilonia Perez <oscar-babilonia@hotmail.com>
2024-05-25 19:31:07 -0300

    check point 2.3

--- exercises/02_basic_calculator/03_if_else/src/lib.rs
+++ exercises/02_basic_calculator/03_if_else/src/lib.rs
@@ -1,6 +1,15 @@
 /// Return `true` if `n` is even, `false` otherwise.
 fn is_even(n: u32) -> bool {
-    todo!()
+    //todo!()
+    let aux1: u32 = n/2u32;
+    let aux2: u32 = (n+1u32)/2u32;
+    let mut resp;
+    if aux1 == aux2 {
+        resp=true;
+    }else{
+        resp=false;
+    }
+    return resp
 }
 
 #[cfg(test)]
